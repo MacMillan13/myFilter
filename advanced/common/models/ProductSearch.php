14b4b26b8e480e96b6865935d38648c190dfc3cd
Vlad <vlad.moiseev1399@gmail.com>
2018-01-18 19:09:57 +0300

    n

--- advanced/common/models/ProductSearch.php
+++ advanced/common/models/ProductSearch.php
@@ -6,7 +6,6 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use common\models\Product;
-use yii\data\Pagination;
 
 /**
  * ProductSearch represents the model behind the search form of `common\models\Product`.
